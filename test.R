73cb574547a862e36ae3ef4d9661adcaee9f669d
Benjamin Chan <benjamin.ks.chan@gmail.com>
2016-03-13 21:48:55 -0700

    Start a test script for secondary malignancies

---
+++ test.R
@@ -0,0 +1,43 @@
+setwd("~/GitHub repositories/AEAfterBreastCaACT/SecondaryMalignancies")
+list.files()
+f <- "SecondaryMalignancies_Data_BG_3_4.xlsx"
+library(openxlsx)
+library(data.table)
+D <- read.xlsx(f, startRow=3, colNames=FALSE)
+D <- data.table(D)
+oldnames <- names(D)
+newnames <- c("id",
+              "authorYear",
+              "trial",
+              "quality",
+              "arm",
+              "nRandomized",
+              "nITT",
+              # "pctCompletingTx",
+              "medianFU",
+              "regimen",
+              "anthracyclineType",
+              "anthracyclineTotalDose",
+              "anthracyclineDuration",
+              "anthracyclineCourses",
+              "cyclophosphamideDose",
+              "cyclophosphamideDuration",
+              "cyclophosphamideCourses",
+              "taxaneType",
+              "taxaneTotalDose",
+              "taxaneDuration",
+              "taxaneCourses",
+              "fluoroucilTotalDose",
+              "fluoroucilDuration",
+              "fluoroucilCourses",
+              "otherTxDetails",
+              "malAML",
+              "malMDS",
+              "malAMLOrMDS",
+              "malNonBreastSolid",
+              "malNonBreastSolidType",
+              "malOtherBloodCancers",
+              "malSMRelatedDeaths",
+              "malSecondPrimary",
+              "NOTES")
+setnames(D, oldnames, newnames[1:33])
