25809e98456522357bf58243355709ab07eb3ead
Benjamin Chan <chanb@ohsu.edu>
2016-08-01 15:57:53 -0700

    Partition into separate RMarkdown files

---
+++ make.R
@@ -0,0 +1,29 @@
+setwd("~/Other/SecMalAfterBreastCaACT")
+
+sink("make.log")
+
+files <- c("preamble.Rmd",
+           "tidyData.Rmd",
+           "summarize.Rmd",
+           "makeAppendix1StudyCharacteristics.Rmd",
+           "metaRegression.Rmd")
+
+f <- file("master.Rmd", open="w")
+for (i in 1:length(files)) {
+    x <- readLines(files[i])
+    writeLines(x, f)
+    if (i < length(files)) {writeLines("\n---\n", f)}
+}
+close(f)
+
+library(knitr)
+library(rmarkdown)
+knit("master.Rmd", output = "index.md")
+file.remove("master.Rmd")
+
+sink("session.log")
+list(completionDateTime = Sys.time(),
+     sessionInfo = sessionInfo())
+sink()
+
+sink()
